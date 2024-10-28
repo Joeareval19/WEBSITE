cea13b69f23a632134f7b50cff812d11e3a8cd5b
Jose Ernesto Arevalo <98182141+Joeareval19@users.noreply.github.com>
2024-10-28 13:47:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const toggleBtn = document.getElementById('toggleBtn');
 const sidebar = document.getElementById('sidebar');
 
+// Add event listener to the toggle button
 toggleBtn.addEventListener('click', () => {
+    // Check the sidebar's current position and toggle it
     if (sidebar.style.left === '-250px') {
         sidebar.style.left = '0'; // Slide in the sidebar
     } else {
